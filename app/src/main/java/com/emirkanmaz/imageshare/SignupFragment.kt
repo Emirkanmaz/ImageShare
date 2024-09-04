22c09d62df916c2bba5f189534513c14cabc111a
Emirkanmaz <emirkan.kanmaz.2001@gmail.com>
2024-09-04 17:34:12 +0300

    post to storage done

--- app/src/main/java/com/emirkanmaz/imageshare/SignupFragment.kt
+++ app/src/main/java/com/emirkanmaz/imageshare/SignupFragment.kt
@@ -8,18 +8,23 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.navigation.Navigation
 import com.emirkanmaz.imageshare.databinding.FragmentSignupBinding
+import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class SignupFragment : Fragment() {
     private var _binding: FragmentSignupBinding? = null
     private val binding get() = _binding!!
     private lateinit var auth: FirebaseAuth
+    private lateinit var db : FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         auth = Firebase.auth
+        db = Firebase.firestore
     }
 
     override fun onCreateView(
@@ -54,18 +59,28 @@ class SignupFragment : Fragment() {
         if (email.isNotBlank() && password.isNotBlank()) {
             auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
                 if (task.isSuccessful) {
-                    Toast.makeText(requireContext(), "Signup Successful", Toast.LENGTH_LONG).show()
-                    val action = SignupFragmentDirections.actionSignupFragmentToFeedFragment()
-                    Navigation.findNavController(view).navigate(action)
+                    val userMap = hashMapOf<String, Any>(
+                        "email" to auth.currentUser!!.email.toString(),
+                        "createdAt" to Timestamp.now()
+                    )
+
+                    db.collection("users").document(auth.currentUser!!.uid).set(userMap)
+                        .addOnSuccessListener {
+                            Toast.makeText(requireContext(), "Signup Successful", Toast.LENGTH_LONG).show()
+
+                            val action = SignupFragmentDirections.actionSignupFragmentToFeedFragment()
+                            Navigation.findNavController(view).navigate(action)
+                        }
+                        .addOnFailureListener { e ->
+                            Toast.makeText(requireContext(), e.localizedMessage, Toast.LENGTH_SHORT).show()
+                        }
+
                 } else {
-                    Toast.makeText(
-                        requireContext(),
-                        task.exception?.localizedMessage.toString(),
-                        Toast.LENGTH_SHORT,
-                    ).show()
+                    Toast.makeText(requireContext(), task.exception?.localizedMessage.toString(), Toast.LENGTH_SHORT).show()
                 }
             }
         }
+
     }
 
     private fun signin(view: View) {
--- app/src/main/java/com/emirkanmaz/imageshare/UploadFragment.kt
+++ app/src/main/java/com/emirkanmaz/imageshare/UploadFragment.kt
@@ -19,10 +19,14 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.emirkanmaz.imageshare.databinding.FragmentUploadBinding
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.ktx.storage
@@ -32,18 +36,20 @@ import java.util.UUID
 class UploadFragment : Fragment() {
     private var _binding: FragmentUploadBinding? = null
     private val binding get() = _binding!!
-    private lateinit var permissionLauncher : ActivityResultLauncher<String>
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
     private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
     private var selectedImage: Uri? = null
     private var selectedBitmap: Bitmap? = null
     private lateinit var auth: FirebaseAuth
     private lateinit var storage: FirebaseStorage
+    private lateinit var db: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         auth = Firebase.auth
         storage = Firebase.storage
-        mediaRegisterLauncher()
+        db = Firebase.firestore
+        registerLaunchers()
     }
 
     override fun onCreateView(
@@ -73,29 +79,6 @@ class UploadFragment : Fragment() {
         binding.postButton.setOnClickListener { postImage(it) }
     }
 
-    private fun postImage(view: View?) {
-        val uuid = UUID.randomUUID()
-        val reference = storage.reference
-        val imageReference = reference.child("images").child("$uuid.jpg")
-        if(selectedImage != null){
-            imageReference.putFile(selectedImage!!).addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    imageReference.downloadUrl.addOnSuccessListener { uri ->
-                        val downloadUrl = uri.toString()
-                        //todo: firestore kayıt
-                    }
-                    Toast.makeText(requireContext(), "Successful", Toast.LENGTH_LONG).show()
-                } else {
-                    Toast.makeText(
-                        requireContext(),
-                        task.exception?.localizedMessage.toString(),
-                        Toast.LENGTH_SHORT,
-                    ).show()
-                }
-            }
-        }
-    }
-
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
@@ -111,6 +94,49 @@ class UploadFragment : Fragment() {
         }
     }
 
+    private fun postImage(view: View) {
+        val uuid = UUID.randomUUID()
+        val reference = storage.reference
+        val imageReference = reference.child("images").child("$uuid.jpg")
+        if (selectedImage != null) {
+            imageReference.putFile(selectedImage!!).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    imageReference.downloadUrl.addOnSuccessListener { uri ->
+                        val imageUrl = uri.toString()
+                        val userDocRef = db.collection("users").document(auth.currentUser!!.uid)
+
+                        val postMap = hashMapOf(
+                            "imageUrl" to imageUrl,
+                            "timestamp" to Timestamp.now(),
+                            "comment" to binding.descriptionEditText.text.toString()
+                        )
+
+                        userDocRef.collection("posts").add(postMap).addOnSuccessListener {
+                            Toast.makeText(
+                                requireContext(),
+                                "Post successfully added",
+                                Toast.LENGTH_LONG).show()
+                            val action = UploadFragmentDirections.actionUploadFragmentToFeedFragment()
+                            Navigation.findNavController(view).navigate(action)
+
+                        }.addOnFailureListener { e ->
+                            Toast.makeText(
+                                requireContext(),
+                                e.localizedMessage,
+                                Toast.LENGTH_LONG).show()
+                        }
+                    }
+                } else {
+                    Toast.makeText(
+                        requireContext(),
+                        task.exception?.localizedMessage.toString(),
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
+        }
+    }
+
     private fun selectImage(view: View) {
         val permission = if (Build.VERSION.SDK_INT >= 33) {
             Manifest.permission.READ_MEDIA_IMAGES
@@ -118,7 +144,11 @@ class UploadFragment : Fragment() {
             Manifest.permission.READ_EXTERNAL_STORAGE
         }
 
-        if (ContextCompat.checkSelfPermission(requireContext(), permission) != PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                permission
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
             if (shouldShowRequestPermissionRationale(permission)) {
                 Snackbar.make(view, "Need Permission", Snackbar.LENGTH_INDEFINITE)
                     .setAction("Give Permission") {
@@ -133,33 +163,43 @@ class UploadFragment : Fragment() {
         }
     }
 
-    private fun mediaRegisterLauncher() {
-        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == AppCompatActivity.RESULT_OK) {
-                result.data?.data?.let { uri ->
-                    selectedImage = uri
-                    try {
-                        selectedBitmap = if (Build.VERSION.SDK_INT >= 29) {
-                            val source = ImageDecoder.createSource(requireActivity().contentResolver, uri)
-                            ImageDecoder.decodeBitmap(source)
-                        } else {
-                            MediaStore.Images.Media.getBitmap(requireActivity().contentResolver, uri)
+    private fun registerLaunchers() {
+        activityResultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == AppCompatActivity.RESULT_OK) {
+                    result.data?.data?.let { uri ->
+                        selectedImage = uri
+                        try {
+                            selectedBitmap = if (Build.VERSION.SDK_INT >= 29) {
+                                val source = ImageDecoder.createSource(
+                                    requireActivity().contentResolver,
+                                    uri
+                                )
+                                ImageDecoder.decodeBitmap(source)
+                            } else {
+                                MediaStore.Images.Media.getBitmap(
+                                    requireActivity().contentResolver,
+                                    uri
+                                )
+                            }
+                            binding.uploadImageView.setImageBitmap(selectedBitmap)
+                        } catch (e: Exception) {
+                            e.printStackTrace()
                         }
-                        binding.uploadImageView.setImageBitmap(selectedBitmap)
-                    } catch (e: Exception) {
-                        e.printStackTrace()
                     }
                 }
             }
-        }
-        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
-            if (granted) {
-                val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-                activityResultLauncher.launch(intent)
-            } else {
-                Toast.makeText(requireContext(), "Permission Denied!", Toast.LENGTH_SHORT).show()
+        permissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
+                if (granted) {
+                    val intent =
+                        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    activityResultLauncher.launch(intent)
+                } else {
+                    Toast.makeText(requireContext(), "Permission Denied!", Toast.LENGTH_SHORT)
+                        .show()
+                }
             }
-        }
     }
 
 }
